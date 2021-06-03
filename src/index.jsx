5d90ed3c0f32a6ac19f94ff4ba69486ef6a1207b
Iva Kubickova <ivakubickova55@gmail.com>
2021-06-03 22:30:04 +0200

    dadjokes-new

---
+++ src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './style.css';
+import { jokes } from './jokes.js';
+import Joke from './Joke';
+
+const App = () => {
+    return (
+        <div className="container">
+            {jokes.map((joke) => (
+                <Joke
+                    key={joke.id}
+                    userAvatar={joke.avatar}
+                    userName={joke.name}
+                    text={joke.text}
+                    likes={joke.likes}
+                    dislikes={joke.dislikes}
+                />
+            ))}
+        </div>
+    );
+};
+
+render(<App />, document.querySelector('#app'));
